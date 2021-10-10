de5e30944a068f3ce93670064b5c62528ebc3ef5
Ricardo Sardón <rst.bcnb@gmail.com>
2021-10-10 20:59:52 +0200

    first styles

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "../Nav"
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>
+
+        <Nav/>
+        
+  </div>;
 };
 
 export default Header;
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return <nav className="navbar">
+    {/*logo*/}
+    <div className="navbar_logo">
+      <h2>THEBRIDGE SHOP</h2>
+    </div>
+    <div className="container-buttons">
+      <button>Name</button>
+      <button>Rating</button>
+      <button>Price</button>
+    </div>
+    <ul className="navbar-links">
+      <Link to="/cart">
+
+      </Link>
+
+    </ul>
+    
+    {/*Hamburguer menu*/}
+    <div className="hamburguer-menu-container">
+
+    </div>
+
+  </nav>;
 };
 
 export default Nav;
